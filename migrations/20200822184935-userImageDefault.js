8a9d2d2dbe1d1a11f796ee601b8ed7e8befb9a7d
Eltonomwega <notelgamwe@gmail.com>
2020-08-22 22:24:48 +0300

    added a default image to userImg

---
+++ migrations/20200822184935-userImageDefault.js
@@ -0,0 +1,28 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+
+    await queryInterface.changeColumn('users','userImg',
+    { 
+      type: Sequelize.STRING,
+      defaultValue:'1595170915110Screenshot (23).png'
+    }
+    );
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+  }
+};
